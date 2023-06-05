0d629f6147b0143d7ac4861861fbea62462c3482
jojosuelobo <josuelobo2000@gmail.com>
2023-06-05 16:35:28 -0300

    Continuação do Contact

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -3,13 +3,31 @@ import styles from './Contact.module.scss'
 import logo from '@/assets/LogoSVG.svg'
 import HeaderCustom from '@/Components/HeaderCustom'
 import FooterCustom from '@/Components/FooterCustom'
+import image from '@/assets/ContactImage.svg'
+import InputFieldCustom from '@/Components/InputFieldCustom'
 
 export default function Contact() {
   return (
     <>
     <HeaderCustom/>
-
-    <FooterCustom/>
+    <div className={styles.infos}>
+      <h1>Quer saber mais a respeito?</h1>
+      <h2>Deixe suas informações conosco em para caso de:</h2>
+      <ul>
+        <li>Demonstração do produto</li>
+        <li>Atualizações do serviço</li>
+        <li>Contato comercial</li>
+        <li>Parcerias</li>
+      </ul>
+      <a>Ou entre em contato via e-mail</a>
+      <p className={styles.email} >agroview@comercial.com.br</p>      
+      <img src={image} className={styles.image} />
+    </div>
+   
+    <form className={styles.form} >
+      {/* <InputFieldCustom text='Nome' /> */}
+    </form>
+    
     </>
   )
 }
