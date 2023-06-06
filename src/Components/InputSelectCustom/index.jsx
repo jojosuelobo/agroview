f2129dfe78c37e4d09fe6a0ae4e6b4d475e41b13
jojosuelobo <josue.lobo@etaure.com.br>
2023-06-06 14:32:18 -0300

    UseStates para select

--- src/Components/InputSelectCustom/index.jsx
+++ src/Components/InputSelectCustom/index.jsx
@@ -5,19 +5,21 @@ import styles from './InputSelectCustom.module.scss'
 
 export default function InputSelectCustom({text}) {
 
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-    ]
+    const [option, setOption] = useState([
+        "",
+        "ESCOLHA 1",
+        "ESCOLHA 2",
+        "ESCOLHA 3",
+        "ESCOLHA 4"
+    ])
+
+    const add = option.map(add => add)
 
     return (
         <div className={styles.text} >
             <p className={styles.label} >{text}</p>
             <select className={styles.input} name={text}>
-                <option value=''></option>
-                <option value='1'>OPÇÃO 1</option>
-                <option value='2'>OPÇÃO 2</option>
+                { add.map((opcao, key) => <option value={key}> {opcao} </option>) }
             </select>
         </div>
     )
