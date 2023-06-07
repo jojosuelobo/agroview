2ce4bac3d2cbabeb477f590616aa92bd8b5426af
jojosuelobo <josuelobo2000@gmail.com>
2023-06-07 16:40:09 -0300

    Correção do footer

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -14,28 +14,30 @@ export default function Contact() {
   return (
     <>
       <HeaderCustom />
-      <div className={styles.infos}>
-        <h1>Quer saber mais a respeito?</h1>
-        <h2>Deixe suas informações conosco em para caso de:</h2>
-        <ul>
-          <li>Demonstração do produto</li>
-          <li>Atualizações do serviço</li>
-          <li>Contato comercial</li>
-          <li>Parcerias</li>
-        </ul>
-        <a>Ou entre em contato via e-mail</a>
-        <p className={styles.email} >agroview@comercial.com.br</p>
-        <img src={image} className={styles.image} />
-      </div>
+      <main className={styles.pai}>
+        <div className={styles.infos}>
+          <h1>Quer saber mais a respeito?</h1>
+          <h2>Deixe suas informações conosco em para caso de:</h2>
+          <ul>
+            <li>Demonstração do produto</li>
+            <li>Atualizações do serviço</li>
+            <li>Contato comercial</li>
+            <li>Parcerias</li>
+          </ul>
+          <a>Ou entre em contato via e-mail</a>
+          <p className={styles.email} >agroview@comercial.com.br</p>
+          <img src={image} className={styles.image} />
+        </div>
 
-      <form className={styles.form} >
-        <InputFieldCustom text='Nome' /> 
-        <InputFieldCustom text='E-mail' /> 
-        <InputSelectCustom text='Tenho interesse em' />
-        <InputFieldCustom text='Observação' obs='obs' /> 
-        <ButtonCustom text='Enviar'/>
-      </form>
-      <FooterCustom/>
+        <form className={styles.form} >
+          <InputFieldCustom text='Nome' />
+          <InputFieldCustom text='E-mail' />
+          <InputSelectCustom text='Tenho interesse em' />
+          <InputFieldCustom text='Observação' obs='obs' />
+          <ButtonCustom text='Enviar' />
+        </form>
+      </main>
+      <FooterCustom />
     </>
   )
 }
